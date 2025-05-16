0feb976512e879b19093a8257059faa38d478ee3
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-16 16:05:15 +0200

    Add add user button and dialog

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  firstName: string;
+  lastName: string;
+  birthDate: number;
+  street: string;
+  zipCode: number;
+  city: string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  position: TooltipPosition = 'above';
+  dialog = inject(MatDialog);
 
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
